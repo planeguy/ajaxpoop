8bd9b6690b651d35b5ae76bad9edabe56732a18a
Delek Turner <delek@delek.org>
2015-02-04 16:39:13 -0800

    refactored into a function that returns a poop for use

--- src/ajaxpoop.js
+++ src/ajaxpoop.js
@@ -1,10 +1,39 @@
+function r(m,u,d,h,c){
+    return new Promise((resolve,rejext)=>{
+        let x  = new XMLHttpRequest();
+        x.open(m.toUpperCase(),u,true);
+        
+        x.onreadystatechange=((e)=>{
+            switch(x.readyState){
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                    break;
+                case 4:
+                    resolve(x);
+            }
+        });
+        x.ontimeout = ((e)=>{
+            reject('timed out');
+        });
+
+
+        let ks=Object.keys(h).filter((j)=>{return h.hasOwnProperty(j);});
+        ks.map((k)=>{
+            x.setRequestHeader(k,h[k]);
+        });
+        if(c) x.withCredentials=c;
+
+        if(d) x.send(d); else x.send();
+    });
+}
+
 class req{
-    constructor(m,u){
-        this.m = m;
+    constructor(u){
         this.u = u;
         this.h = {};
         this.c = false;
-        this.q = {}
     }
     header(k,v){
         this.h.k=v;
@@ -14,57 +43,22 @@ class req{
         this.c=c;
         return this;
     }
-    query(k,v){
-        q[k]=v;
+    get(){
+        return r('GET', this.u,  undefined, this.h, this.c);
     }
-    send(d){
-        return ((m,u,q,d,h,c)=>{
-            return new Promise((resolve,rejext)=>{
-                let x  = new XMLHttpRequest()
-                    ks, qry='';
-                x.onreadystatechange((e)=>{
-                    switch(x.readyState){
-                    case 0:
-                    case 1:
-                    case 2:
-                    case 3:
-                        break;
-                    case 4:
-                        resolve(x);
-                    }
-                });
-
-                ks=Object.keys(q).filter((j)=>{return q.hasOwnProperty(j);});
-                if(ks.length>0){
-                    qry+='?';
-                    ks.foreach((k)=>{
-                        qry+=k+'='+uriEncodeComponent(q[k]);
-                    });
-                }
-                ks=Object.keys(h).filter((j)=>{return q.hasOwnProperty(j);});
-                ks.foreach((k)=>{
-                    x.setRequestHeader(k,h[k]);
-                });
-                if(c) x.withCredentials=c;
-
-                x.open(m.toUpperCase(),u+q,true);
-                if(d) x.send(d); else x.send();
-            });
-        })(this.m, this.u, this.q, d, this.h, this.c);
+    put(d){
+        if(!this.h['Content-Type']) this.h['Content-Type']='application/json';
+        return r('PUT', this.u, d, this.h, this.c);
+    }
+    post(d){
+        if(!this.h['Content-Type']) this.h['Content-Type']='application/json';
+        return r('POST', this.u, d, this.h, this.c);
+    }
+    delete(){
+        return r('DELETE', this.u, undefined, this.h, this.c);
     }
 }
 
-export default poop {
-    get:(u)=>{
-        return new req('GET',u);
-    },
-    put:(u)=>{
-        return new req('PUT',u).header('Content-Type','application/json');
-    },
-    post:(u)=>{
-        return new req('POST',u).header('Content-Type','application/json');
-    },
-    delete:(u)=>{
-        return new req('DELETE',u);
-    }
-};
+export default function poop(u){
+    return new req(u)
+}
